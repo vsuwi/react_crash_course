b0cbee82cd6d088beed8dd9103b390b9655bf5ac
vsuwi solano <victordsolano@gmail.com>
2024-06-18 19:07:13 +0000

    fix the listings showing

--- my-react-app-2024/src/components/JobListings.jsx
+++ my-react-app-2024/src/components/JobListings.jsx
@@ -1,13 +1,32 @@
-import jobs from '../jobs.json'
+// import jobs from '../jobs.json'
 import JobListing from './JobListing'
-
+import { useState, useEffect } from 'react';
 
 
 const JobListings = ({isHome = false}) => {
-    const jobListings = isHome ? jobs.slice(0,3) : jobs;
+    // const jobListings = isHome ? jobs.slice(0,3) : jobs;
     // console.log(jobs)
 
-    
+    const [jobs, setJobs] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+      const fecthJobs = async () => {
+        try{
+          console.log("im in the try")
+          const res = await fetch('http://localhost:3000/jobs')//error here prob since im using gitpod
+          console.log("im after res")
+          const data = await res.json();
+          setJobs(data);
+        } catch(error){
+          console.log('error getting data', error)
+        } finally{
+          setLoading(false);
+        }
+      }
+
+      fecthJobs();
+    }, []);
 
   return (
         <section className="bg-blue-50 px-4 py-10">
@@ -17,7 +36,7 @@ const JobListings = ({isHome = false}) => {
             </h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
 
-                    {jobListings.map((job) => (
+                    {jobs.map((job) => (
                         <JobListing job = {job} key = {job.id}></JobListing>
                     ))}
                     
